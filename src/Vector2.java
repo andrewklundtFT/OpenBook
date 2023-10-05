675edc0aa677609be9f2ef33ac80f42fc2533657
andrewklundtFT <ak1036178@student.francistuttle.edu>
2023-10-05 14:29:34 -0500

    Added isEqual demonstration

--- src/Vector2.java
+++ src/Vector2.java
@@ -5,6 +5,8 @@ public class Vector2 {
         System.out.println(a + " a magnitude: " + a.magnitude());
         System.out.println(b + " b magnitude: " + b.magnitude());
         System.out.println("a plus b = " + a.add(b));
+        System.out.println("is a equal to b? " + a.equals(b));
+
     }
 
     private int x;
@@ -15,17 +17,33 @@ public class Vector2 {
         y = userY;
     }
 
+    public int getX () {
+        return x;
+    }
+
+    public int getY () {
+        return y;
+    }
+
+    public void setX (int userX) {
+        x = userX;
+    }
+
+    public void setY (int userY) {
+        y = userY;
+    }
+
     public double magnitude () {
         return Math.sqrt((x * x) + (y * y));
     }
 
     public Vector2 add (Vector2 b) {
-        Vector2 result = new Vector2(x + b.x, y + b.y);
+        Vector2 result = new Vector2(x + b.getX(), y + b.getY());
         return result;
     }
 
     public boolean isEqual (Vector2 b) {
-        if (x == b.x && y == b.y) {
+        if (x == b.getX() && y == b.getY()) {
             return true;
         }
         return false;
